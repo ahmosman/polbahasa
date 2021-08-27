824a395e43bf500d283cd1f994daa6d0d84f618a
ahmix <65831872+ahmix@users.noreply.github.com>
2021-08-27 16:04:23 +0200

    changed fonts

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -21,7 +21,6 @@ class AuthController extends AbstractController
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        dump($error);
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
