e5121f88d591a9ac8a8fb9e4d2be757cbe642cbc
ahmosman <ahmedosman.inf@gmail.com>
2022-08-09 21:18:41 +0200

    fixed hiding suggestions when search input is not focused

--- assets/js/search_suggestions.js
+++ assets/js/search_suggestions.js
@@ -7,39 +7,50 @@ let searchButton = document.querySelector('.search-input>button')
 
 suggestionsEvents.suggestionsScroller(searchInput, suggestionsUl)
 
-searchInput.addEventListener('input', () => {
-  if (searchInput.value.length > 0) {
-    suggestionsDiv.classList.remove('opacity0')
-    suggestionsUl.classList.remove('hidden')
-    searchSuggestions()
-  } else {
-    suggestionsDiv.classList.add('opacity0')
-    suggestionsUl.classList.add('hidden')
-  }
+searchInput.addEventListener('focus', () => {
+    suggestionsDiv.classList.remove('hidden')
 })
 
-async function searchSuggestions () {
-  let response = await fetch(`${Routing.generate('autocomplete')}?all=t&q=${searchInput.value}`)
-  let words = await response.json()
-  words.sort(function (a, b) {
-    return a.length - b.length
-  })
-  suggestionsUl.innerHTML = ''
-  for (const word of words) {
-    suggestionsUl.innerHTML += `<li>${word}</li>`
-  }
-  let suggestionsLi = suggestionsUl.querySelectorAll('li')
-  for (const li of suggestionsLi) {
-    li.addEventListener('click', () => {
-      searchInput.value = li.textContent
-      searchButton.click()
+searchInput.addEventListener('blur', () => {
+    suggestionsDiv.classList.add('hidden')
+})
+
+searchInput.addEventListener('input', () => {
+    if (searchInput.value.length > 0) {
+        suggestionsUl.classList.remove('hidden')
+        searchSuggestions().then(words => updateSuggestions(words))
+    } else {
+        suggestionsUl.innerHTML = ''
+        suggestionsUl.classList.add('hidden')
+    }
+})
+
+async function searchSuggestions() {
+    let response = await fetch(`${Routing.generate('autocomplete')}?all=t&q=${searchInput.value}`)
+    let suggestions = await response.json()
+    suggestions.sort(function (a, b) {
+        return a.length - b.length
     })
-  }
-  suggestionsEvents.suggestionsMouseEvents(suggestionsUl)
-  suggestionsUl = document.querySelector('.search-suggestions-ul')
-  if (suggestionsUl.offsetHeight >= 185) {
-    suggestionsUl.classList.add('search-suggestions-scroll')
-  } else {
-    suggestionsUl.classList.remove('search-suggestions-scroll')
-  }
+    return suggestions
+}
+
+function updateSuggestions(suggestions) {
+    suggestionsUl.innerHTML = ''
+    for (const suggestion of suggestions) {
+        suggestionsUl.innerHTML += `<li>${suggestion}</li>`
+    }
+    let suggestionsLi = suggestionsUl.querySelectorAll('li')
+    for (const li of suggestionsLi) {
+        li.addEventListener('mousedown', () => {
+            searchInput.value = li.textContent
+            searchButton.click()
+        })
+    }
+    suggestionsEvents.suggestionsMouseEvents(suggestionsUl)
+    suggestionsUl = document.querySelector('.search-suggestions-ul')
+    if (suggestionsUl.offsetHeight >= 185) {
+        suggestionsUl.classList.add('search-suggestions-scroll')
+    } else {
+        suggestionsUl.classList.remove('search-suggestions-scroll')
+    }
 }
